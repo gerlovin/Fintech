1fb2441d47c56bb80bb55717120997d67d10ada6
Anna <livshitsanna@gmail.com>
2023-08-17 19:57:08 +0300

    First sql query in Rep

--- src/main/java/telran/java47/fintech/dao/StockRepository.java
+++ src/main/java/telran/java47/fintech/dao/StockRepository.java
@@ -1,6 +1,7 @@
 package telran.java47.fintech.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import telran.java47.communication.dto.TimeHistoryLimitsForIndexDto;
 import telran.java47.fintech.model.Stock;
@@ -8,5 +9,8 @@ import telran.java47.fintech.model.StockKey;
 
 public interface StockRepository extends JpaRepository<Stock, StockKey> {
 	
-//	TimeHistoryLimitsForIndexDto timeLimits(String id);
+	@Query("select new telran.java47.communication.dto.TimeHistoryLimitsForIndexDto( ?1, MIN(s.stockKey.dateStock), MAX(s.stockKey.dateStock) ) from Stock s where s.name LIKE ?1")
+	TimeHistoryLimitsForIndexDto timeLimits(String name);
+	
+
 }
