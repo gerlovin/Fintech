26f9d0bd8914c71e577382f34ebfb9c0ea7226fe
Girllovin <gerllovin@gmail.com>
2023-08-17 20:36:24 +0300

    Web_Client

--- src/main/java/telran/java47/communication/dto/PeriodBeetwinDto.java
+++ src/main/java/telran/java47/communication/dto/PeriodBeetwinDto.java
@@ -2,10 +2,16 @@ package telran.java47.communication.dto;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+import lombok.Setter;
+@Builder
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
