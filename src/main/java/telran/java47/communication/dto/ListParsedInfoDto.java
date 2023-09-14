6af958a24a6ff19bfb30f04d24724decb9ebdc5b
Girllovin <gerllovin@gmail.com>
2023-09-14 21:05:21 +0300

    New loader

--- src/main/java/telran/java47/communication/dto/ListParsedInfoDto.java
+++ src/main/java/telran/java47/communication/dto/ListParsedInfoDto.java
@@ -2,6 +2,17 @@ package telran.java47.communication.dto;
 
 import java.util.HashMap;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ListParsedInfoDto {
      HashMap<String, ParsedInfoDto> mapStocks;
 }
