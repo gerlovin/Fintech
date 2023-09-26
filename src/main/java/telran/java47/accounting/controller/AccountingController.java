98562e48a4ec2134f4a37aa4c8dc3e5a19b1c6df
Anna <livshitsanna@gmail.com>
2023-09-26 12:28:00 +0300

    receive queries to accounting from all addresses

--- src/main/java/telran/java47/accounting/controller/AccountingController.java
+++ src/main/java/telran/java47/accounting/controller/AccountingController.java
@@ -3,6 +3,7 @@ package telran.java47.accounting.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,23 +25,27 @@ public class AccountingController {
 	final UserAccountService userAccountService;
 	
 	@PostMapping("/account/register")
+	@CrossOrigin(origins = "*") 
 	public UserDto register(@RequestBody UserRegisterDto userRegisterDto) {
 		return userAccountService.register(userRegisterDto);
 		
 	}
 	
-	@DeleteMapping("/account/user/{login}")
-	public UserDto removeUser(@PathVariable String login) {
-		return userAccountService.removeUser(login);
+	@DeleteMapping("/account/user/{email}")
+	@CrossOrigin(origins = "*") 
+	public UserDto removeUser(@PathVariable String email) {
+		return userAccountService.removeUser(email);
 	}
 	
 	
 	@GetMapping("/account/users")
+	@CrossOrigin(origins = "*") 
 	public List<UserInfoDto> getAllUsers() {
 		return userAccountService.getAllUsers();
 	}
 	
 	@GetMapping("/account/recovery/{email}")
+	@CrossOrigin(origins = "*") 
 	@ResponseStatus(HttpStatus.ACCEPTED)
 	public void getRecovery(@PathVariable String email) {
 		System.out.println("we are here");
--- src/main/java/telran/java47/accounting/service/UserAccountServiceImpl.java
+++ src/main/java/telran/java47/accounting/service/UserAccountServiceImpl.java
@@ -14,7 +14,9 @@ import telran.java47.accounting.dto.UserRegisterDto;
 import telran.java47.accounting.dto.exceptions.UserExistsException;
 import telran.java47.accounting.dto.exceptions.UserNotFoundException;
 import telran.java47.accounting.model.UserAccount;
-
+enum Roles {
+    ADMIN, MODERATOR, USER
+}
 
 @Service
 @RequiredArgsConstructor
@@ -34,38 +36,37 @@ public class UserAccountServiceImpl implements UserAccountService {
 		UserAccount userAccount = modelMapper.map(userRegisterDto, UserAccount.class);
 		//String password = passwordEncoder.encode(userRegisterDto.getPassword());
 		//userAccount.setPassword(password);
-		userAccount.addRole("USER");
+		userAccount.addRole(Roles.USER.toString());
 		userAccountRepository.save(userAccount);
 		userAccountRepository.findAll().forEach(e -> System.out.println(e));
 		return modelMapper.map(userAccount, UserDto.class);
 	}
 
 	@Override
-	public UserDto getUser(String login) {
+	public UserDto getUser(String email) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public UserDto removeUser(String login) {
-		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+	public UserDto removeUser(String email) {
+		UserAccount userAccount = userAccountRepository.findById(email).orElseThrow(UserNotFoundException::new);
 		userAccountRepository.delete(userAccount);
 		return modelMapper.map(userAccount, UserDto.class);
 	}
 
 	@Override
-	public void changePassword(String login, String newPassword) {
+	public void changePassword(String email, String newPassword) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public List<UserInfoDto> getAllUsers() {
-//		
-//		return userAccountRepository.findAll().stream()
-//				.map(u -> modelMapper.map(u, UserInfoDto.class))
-//				.toList();
-		return null;
+
+		return userAccountRepository.findAll().stream()
+				.map(u -> modelMapper.map(u, UserInfoDto.class))
+				.toList();
 	}
 
 	@Override
@@ -82,6 +83,21 @@ public class UserAccountServiceImpl implements UserAccountService {
 		System.out.println("User " + email + " exists") ;
 		emailService.sendEmail(email, "New password for Fintech app", "Your new password is " + passwordNew);
 		
+	}
+
+	@Override
+	public UserDto changeRolesList(String email, String role, boolean isAddRole) {
+		UserAccount userAccount = userAccountRepository.findById(email.trim()).orElse(null);
+		if (userAccount == null) {
+			throw new UserNotFoundException();
+		}
+		if (isAddRole) {
+			userAccount.addRole(role);
+		}else {
+			userAccount.removeRole(role);
+		}
+		userAccountRepository.save(userAccount);
+		return modelMapper.map(userAccount, UserDto.class);
 	}
 
 }
